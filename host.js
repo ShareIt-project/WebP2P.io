fa2c4f3d21ad650a96c1dd7999954bffc7a630c7
Jesús Leganés Combarro "Piranna <piranna@gmail.com>
2012-09-27 00:36:50 +0200

    Removed host.js

--- host.js
+++
@@ -1,6 +0,0 @@
-// Filereader support (be able to host files from the filesystem)
-if(typeof FileReader == "undefined")
-	oldBrowser();
-
-
-var chunksize = 65536
--- transport_peer.js
+++ transport_peer.js
@@ -1,3 +1,11 @@
+// Filereader support (be able to host files from the filesystem)
+if(typeof FileReader == "undefined")
+    oldBrowser();
+
+
+var chunksize = 65536
+
+
 function Transport_Peer_init(transport, db, host)
 {
     transport._send_files_list = function(filelist)
