b2cd9724d45af8a256d7468b649af1c58210b7ee
Jesús Leganés Combarro "Piranna <piranna@gmail.com>
2012-10-01 01:47:41 +0200

    Using EventTarget.js

--- transport/index.js
+++ transport/index.js
@@ -2,40 +2,7 @@ function Transport_init(transport, onsuccess)
 {
     transport.onopen = function()
     {
-	    var protocol = {}
-
-	    // EventTarget interface
-	    protocol._events = {};
-
-	    protocol.addEventListener = function(type, listener)
-	    {
-	      protocol._events[type] = protocol._events[type] || [];
-	      protocol._events[type].push(listener);
-	    };
-
-	    protocol.dispatchEvent = function(type)
-	    {
-	      var events = protocol._events[type];
-	      if(!events)
-	        return;
-
-	      var args = Array.prototype.slice.call(arguments, 1);
-
-	      for(var i = 0, len = events.length; i < len; i++)
-	        events[i].apply(null, args);
-	    };
-
-        protocol.removeEventListener = function(type, listener)
-        {
-          var events = protocol._events[type];
-          if(!events)
-            return;
-
-          events.splice(events.indexOf(listener), 1)
-
-          if(!events.length || !listener)
-            delete protocol._events[type]
-        };
+	    var protocol = new EventTarget()
 
         // Compose and send message
 	    protocol.emit = function()
@@ -54,7 +21,10 @@ function Transport_init(transport, onsuccess)
 	    {
 	        console.log("transport.onmessage = '"+message.data+"'")
 
-            protocol.dispatchEvent.apply(protocol, JSON.parse(message.data))
+            message = JSON.parse(message.data)
+            var event = {'type': message[0], 'data': message.slice(1)}
+
+            protocol.dispatchEvent(event)
 	    }
 
 	    if(onsuccess)
