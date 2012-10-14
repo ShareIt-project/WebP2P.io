0397df1b6251ec1057dcf2ae6cf352d065d843f4
Jesús Leganés Combarro "Piranna <piranna@gmail.com>
2012-10-14 12:59:59 +0200

    Put hasher code on an independent object

---
+++ hasher.js
@@ -0,0 +1,20 @@
+function Hasher(db)
+{
+    var worker = new Worker('js/webp2p/hasher_worker.js');
+        worker.onmessage = function(event)
+        {
+            db.files_add(event.data)
+
+            if(this.onsuccess)
+                this.onsuccess()
+        }
+
+    this.hash = function(fileslist)
+    {
+        worker.postMessage(fileslist)
+
+        // Loop through the FileList and add sharedpoints to list.
+        for(var i=0, file; file=fileslist[i]; i++)
+            db.sharepoints_add(file)
+    }
+}
