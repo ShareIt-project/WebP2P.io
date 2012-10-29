91747824a622f734950d3c3315fb2e1d0342a0f0
Jesús Leganés Combarro "Piranna <piranna@gmail.com>
2012-10-29 23:12:26 +0100

    Added mock-up of SIP signaling from JsSIP demo

--- transport/signaling/sip.js
+++ transport/signaling/sip.js
@@ -1,3 +1,65 @@
 function Transport_Signaling_SIP_init(signaling, peersManager)
 {
+    var configuration =
+    {
+      'outbound_proxy_set': [outbound_proxy_set],
+      'uri': sip_uri,
+      'display_name': '',
+      'password':  sip_password,
+      'register_expires': 600,
+      'secure_transport': false,
+      'stun_server': 'aliax.net',
+      'trace_sip': true,
+      'hack_ip_in_contact': true,
+      'hack_via_tcp': true
+    };
+
+    var ua
+
+    try
+    {
+        ua = new JsSIP.UA(configuration);
+    }
+    catch(e)
+    {
+      console.log(e);
+      return;
+    }
+
+    // Transport connection/disconnection callbacks
+    ua.on('connected', ws_connected);
+    ua.on('disconnected', ws_disconnected);
+
+    // Call/Message reception callbacks
+    ua.on('newSession', function(e)
+    {
+      GUI.new_session(e)
+    });
+
+    ua.on('newMessage', function(e)
+    {
+      GUI.new_message(e)
+    });
+
+    // Registration/Deregistration callbacks
+    ua.on('registered', function(e)
+    {
+      console.info('Registered');
+      GUI.setStatus("registered");
+    });
+
+    ua.on('unregistered', function(e)
+    {
+      console.info('Deregistered');
+      GUI.setStatus("connected");
+    });
+
+    ua.on('registrationFailed', function(e)
+    {
+      console.info('Registration failure');
+      GUI.setStatus("connected");
+    });
+
+    // Start
+    ua.start();
 }
