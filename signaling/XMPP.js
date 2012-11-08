b7a68c85223a23f721e7e920649c5816038a6d87
Jesús Leganés Combarro "Piranna <piranna@gmail.com>
2012-11-08 15:49:01 +0100

    Adapted XMPP signaling to new style

--- signaling/XMPP.js
+++ signaling/XMPP.js
@@ -8,38 +8,38 @@ function Signaling_XMPP(configuration, manager)
         signaling.registerHandler('onconnect', function(e)
         {
             // Compose and send message
-            self.emit = function(id, sdp)
+            self.emit = function(uid, data)
             {
-                var args = Array.prototype.slice.call(arguments, 0);
-                var peer = args[1]
-                args.splice(1,1)
-
                 var oMsg = new JSJaCMessage();
-                    oMsg.setTo(new JSJaCJID(peer));
-                    oMsg.setBody(JSON.stringify(args));
+                    oMsg.setTo(new JSJaCJID(uid));
+                    oMsg.setBody(JSON.stringify(data));
 
                 signaling.send(oMsg);
             }
 
             signaling.registerHandler('message', function(oJSJaCPacket)
             {
-                var peer    = oJSJaCPacket.getFromJID()
-                var message = JSON.parse(oJSJaCPacket.getBody())
+                var uid  = oJSJaCPacket.getFromJID()
+                var data = JSON.parse(oJSJaCPacket.getBody())
 
-                switch(message[0])
+                switch(data[0])
                 {
                     case 'offer':
                         if(manager.onoffer)
-                            manager.onoffer(peer, message[1])
+                            manager.onoffer(uid, data[1])
                         break
 
                     case 'answer':
                         if(manager.onanswer)
-                            manager.onanswer(peer, message[1])
+                            manager.onanswer(uid, data[1])
                 }
             })
 
             signaling.send(new JSJaCPresence());
+
+            // Set signaling as open
+            if(self.onopen)
+                self.onopen(configuration.username)
         });
         signaling.registerHandler('onerror', function(event)
         {
