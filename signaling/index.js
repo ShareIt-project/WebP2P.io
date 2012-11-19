b6b10249b404ae30efa4c660732288535b4ef856
Jesús Leganés Combarro "Piranna <piranna@gmail.com>
2012-11-19 21:43:21 +0100

    SignalingManager is able to select between several signaling channels

--- signaling/index.js
+++ signaling/index.js
@@ -1,59 +1,75 @@
 function SignalingManager(configuration)
 {
-    var index = Math.floor(Math.random()*configuration.length)
-    var index = 2   // Fixed value until I get SIP and/or XMPP to work
-
-    var type = configuration[index][0]
-    var conf = configuration[index][1]
-
-    switch(type)
-    {
-        case 'SimpleSignaling':
-            var signaling = new Signaling_SimpleSignaling(conf)
-            break;
-
-        case 'SIP':
-            var signaling = new Signaling_SIP(conf)
-            break;
-
-        case 'XMPP':
-            var signaling = new Signaling_XMPP(conf)
-    }
-
     var self = this
 
-    signaling.onopen = function(uid)
-    {
-        signaling.onmessage = function(uid, data)
-        {
-            switch(data[0])
-            {
-                case 'offer':
-                    if(self.onoffer)
-                        self.onoffer(uid, data[1])
-                    break
+    var signaling = null
 
-                case 'answer':
-                    if(self.onanswer)
-                        self.onanswer(uid, data[1])
-            }
+    function getRandomSignaling(configuration)
+    {
+        var index = Math.floor(Math.random()*configuration.length)
+
+        var type = configuration[index][0]
+        var conf = configuration[index][1]
+
+        switch(type)
+        {
+            case 'SimpleSignaling':
+                signaling = new Signaling_SimpleSignaling(conf)
+                break;
+
+            case 'SIP':
+                signaling = new Signaling_SIP(conf)
+                break;
+
+            case 'XMPP':
+                signaling = new Signaling_XMPP(conf)
         }
 
-        if(self.onUID)
-           self.onUID(uid)
+        signaling.onopen = function(uid)
+        {
+            signaling.onmessage = function(uid, data)
+            {
+                switch(data[0])
+                {
+                    case 'offer':
+                        if(self.onoffer)
+                            self.onoffer(uid, data[1])
+                        break
+
+                    case 'answer':
+                        if(self.onanswer)
+                            self.onanswer(uid, data[1])
+                }
+            }
+
+            if(self.onUID)
+               self.onUID(uid)
+        }
+        signaling.onerror = function(error)
+        {
+            console.error(error)
+
+            // Try to get an alternative signaling channel
+            configuration.splice(index, 1)
+            getRandomSignaling(configuration)
+        }
     }
-    signaling.onerror = function(error)
-    {
-        console.error(error)
-    }
+
+    getRandomSignaling(configuration)
 
     this.sendOffer = function(uid, sdp)
     {
-        signaling.send(uid, ["offer", sdp]);
+        if(signaling)
+            signaling.send(uid, ["offer", sdp]);
+        else
+            console.warning("signaling is not available");
     }
 
     this.sendAnswer = function(uid, sdp)
     {
-        signaling.send(uid, ["answer", sdp]);
+        if(signaling)
+            signaling.send(uid, ["answer", sdp]);
+        else
+            console.warning("signaling is not available");
     }
 }
