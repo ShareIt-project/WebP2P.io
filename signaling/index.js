1ae5b3df699cd265255474c89ed8fbfad5e30496
Jesús Leganés Combarro "Piranna <piranna@gmail.com>
2012-11-20 18:46:19 +0100

    UID is autogenerated on SignalingManager if none is specified

--- signaling/index.js
+++ signaling/index.js
@@ -4,6 +4,8 @@ function SignalingManager(configuration)
 
     var signaling = null
 
+    var UUIDv4 = function b(a){return a?(a^Math.random()*16>>a/4).toString(16):([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g,b)}
+
     function getRandomSignaling(configuration)
     {
         var index = Math.floor(Math.random()*configuration.length)
@@ -14,14 +16,17 @@ function SignalingManager(configuration)
         switch(type)
         {
             case 'SimpleSignaling':
+                conf.uid = conf.uid || UUIDv4()
                 signaling = new Signaling_SimpleSignaling(conf)
                 break;
 
             case 'SIP':
+                conf.uri = conf.uri || UUIDv4()+'@'+conf.outbound_proxy_set
                 signaling = new Signaling_SIP(conf)
                 break;
 
             case 'XMPP':
+                conf.username = conf.username || UUIDv4()
                 signaling = new Signaling_XMPP(conf)
         }
 
