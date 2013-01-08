be07cfb902b2ab01d7e9ebcb2d1343ad11c4d963
Jesús Leganés Combarro "Piranna <piranna@gmail.com>
2013-01-08 03:46:11 +0100

    Added Dropbox basic authentication code

--- hasher/index.js
+++ hasher/index.js
@@ -158,6 +158,26 @@ function Hasher(db, policy)
     dropboxClient = new Dropbox.Client({key: "iCoCBMCCU3A=|YyQQmo4XMmB5hH9JOrXLCcIH6ike807VOEwpHsdcWA==",
                                         sandbox: true});
     dropboxClient.authDriver(new Dropbox.Drivers.Popup({receiverFile: "oauth_receiver.html"}));
+    dropboxClient.authenticate(function(error, client)
+    {
+        if(error)
+        {
+          // Replace with a call to your own error-handling code.
+          //
+          // Don't forget to return from the callback, so you don't execute the code
+          // that assumes everything went well.
+//          return showError(error);
+
+            console.error(error)
+            dropboxClient = null
+        }
+
+        // Replace with a call to your own application code.
+        //
+        // The user authorized your app, and everything went well.
+        // client is a Dropbox.Client instance that you can use to make API calls.
+//        doSomethingCool(client);
+      });
     // Dropbox plugin end
 
     /**
